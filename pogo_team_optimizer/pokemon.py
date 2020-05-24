11245d5633b0b2c5de847c9dc67328d41ef971c7
Xiaojian "JJ" Deng <xjd001@gmail.com>
2020-05-23 21:50:55 -0500

    Update pokemon.py

--- pogo_team_optimizer/pokemon.py
+++ pogo_team_optimizer/pokemon.py
@@ -1,3 +1,4 @@
+from abc import ABC
 
 class Pokemon(object):
     
@@ -8,11 +9,31 @@ class Pokemon(object):
         self.nickname = nickname
         self.type = identifier.split("-")[0]
         
-class PokemonCollection(object):
-    pass
+class PokemonCollection(ABC):
+    
+    def __init__(self):
+        self.pokemons = []
+        self.types = set()
+    
+    def add(self, pokemon):
+        self.pokemons.append(pokemon)
+        self.types.add(pokemon.type)
 
 class Roster(PokemonCollection):
-    pass
+    
+    def create_lineups(self):
+        pass
 
 class Lineup(PokemonCollection):
+    
+    def raise_for_validation(self):
+        if len(self.pokemons) != 3:
+            raise InvalidLineupException
+        if len(self.types) != 3:
+            raise InvalidLineupException
+            
+    def get_rating(self):
+        pass
+            
+class InvalidLineupException(Exception):
     pass
